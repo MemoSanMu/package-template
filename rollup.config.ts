f267c239ee53757e4c518b6e766898c93c3f2c3f
王森 <wangsen1@yonghui.cn>
2022-01-19 16:29:33 +0800

    feat(docs): update

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,19 +2,20 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
+import typescript from '@rollup/plugin-typescript'
 import json from 'rollup-plugin-json'
 import babel from '@rollup/plugin-babel'
 import filesize from 'rollup-plugin-filesize'
+// import { terser } from 'rollup-plugin-terser'
 
 const pkg = require('./package.json')
 
-const libraryName = pkg.name
+const libraryName = camelCase(pkg.name)
 
 export default {
   input: `src/index.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true }, // 通用模块定义，以amd，cjs和iife为一体
+    { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true }, // 通用模块定义，以amd，cjs和iife为一体
     { file: pkg.module, format: 'es', sourcemap: true } // ES模块文件
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
@@ -26,7 +27,7 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript(),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
@@ -39,9 +40,11 @@ export default {
       babelHelpers: 'bundled' // 开启体积优化
     }),
 
-    // Resolve source maps to the original source
-    sourceMaps(),
+    // terser(),
 
-    filesize()
+    filesize(),
+
+    // Resolve source maps to the original source
+    sourceMaps()
   ]
 }
